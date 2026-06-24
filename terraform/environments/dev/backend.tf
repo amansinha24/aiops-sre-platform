f372749302e9e83bc1bd526b59d7b0dd55ab70f0
Aman Sinha <amank494@gmail.com>
2026-06-24 17:23:56 +0530

    complete phase 2 - VPC, EKS, ECR infrastructure

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "aiops-terraform-state-350480401763"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "aiops-terraform-locks"
+    use_lockfile   = true
     encrypt        = true
   }
 }
